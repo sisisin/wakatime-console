8c4a0fc8a21dd3d5949c807a5a668f4c1730c80e
sisisin <azsisisin@gmail.com>
2016-10-08 20:03:43 +0900

    feat(model) generate Language.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,7 @@
 class Project < ApplicationRecord
   has_many :editor, dependent: :destroy
   has_many :entity, dependent: :destroy
+  has_many :language, dependent: :destroy
   validates :date, presence: true, uniqueness: { scope: [:name] }
   validates :name, presence: true
   validates :total_seconds, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161008105621) do
+ActiveRecord::Schema.define(version: 20161008110154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,15 @@ ActiveRecord::Schema.define(version: 20161008105621) do
     t.index ["project_id"], name: "index_entities_on_project_id", using: :btree
   end
 
+  create_table "languages", force: :cascade do |t|
+    t.integer  "project_id",    null: false
+    t.string   "name",          null: false
+    t.integer  "total_seconds", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.index ["project_id"], name: "index_languages_on_project_id", using: :btree
+  end
+
   create_table "projects", force: :cascade do |t|
     t.date     "date"
     t.string   "name"
@@ -43,4 +52,5 @@ ActiveRecord::Schema.define(version: 20161008105621) do
 
   add_foreign_key "editors", "projects"
   add_foreign_key "entities", "projects"
+  add_foreign_key "languages", "projects"
 end
