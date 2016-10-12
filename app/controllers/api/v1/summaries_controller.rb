652f3288d038d5e4919306d22269596b5cd05342
sisisin <azsisisin@gmail.com>
2016-10-13 02:50:45 +0900

    feat(api) add exclude condition in DB request

--- app/controllers/api/v1/summaries_controller.rb
+++ app/controllers/api/v1/summaries_controller.rb
@@ -1,9 +1,14 @@
+require_relative '../../concerns/exclude_repository'
+
 class Api::V1::SummariesController < ApplicationController
   def index
+    exclude_repos = ExcludeRepository.new.get_exclude_repos
     @project = Project
       .includes(:editors, :entities, :languages, :operating_systems)
       .references(:editors, :entities, :languages, :operating_systems)
-      .find_by(date: Date::today - 1)
+      .where(date: Date::today - 1)
+      .where.not(name: exclude_repos)
+      .first
       .to_json(:include => [:editors, :entities, :languages, :operating_systems])
 
     render json: @project
