dfacdb5edaa4a744dbe8c1621402ea761863fd76
sisisin <azsisisin@gmail.com>
2016-10-13 03:37:44 +0900

    test(model) fix failure test

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,7 +3,7 @@ class Project < ApplicationRecord
   has_many :entities, dependent: :destroy
   has_many :languages, dependent: :destroy
   has_many :operating_systems, dependent: :destroy
-  validates :date, presence: true, uniqueness: { scope: [:name] }
+  validates :date, presence: true, uniqueness: { scope: :name }
   validates :name, presence: true
   validates :total_seconds, presence: true
 end
